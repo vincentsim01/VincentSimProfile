a48a39d337d89e7a8ea7b9711f69c2365f57b8cb
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-08-24 08:44:08 +0800

    snapshot

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ secondMagicButton.addEventListener("click", alert("secondMagicButton")
 );
 */
 
-//setInterval(removeImage,5000);
 
 function removeImage(){
 
@@ -47,12 +46,36 @@ function removeImage(){
 
 }
 
+let i=0;
 
-/*
-function addBlurryImage(){
-    changeImageButton.classList.remove("newImage");
-    changeImageButton.classList.add("noImage");
+var aboutMePictureButton=document.getElementById("aboutMePictureButton");
 
-}
-*/
+var aboutMePicture = document.getElementById("theAboutMePicture");
+
+var arrayAboutMePicture=["./Image/SG.JPG","./Image/SG2.JPG","./Image/SG3.JPG","./Image/SG4.JPG","./Image/SG5.JPG"];
+var AboutMePictureAttribute=aboutMePicture.getAttribute("src");
+
+console.log(AboutMePictureAttribute);
+console.log(arrayAboutMePicture[1]);
+
+aboutMePictureButton.addEventListener("click",function(){
+    if(i>=arrayAboutMePicture.length-1){
+        aboutMePicture.setAttribute("src",arrayAboutMePicture[0]);
+        i=0;
+
+    }else if(i<arrayAboutMePicture.length-1){
+        i=i+1;
+
+        aboutMePicture.setAttribute("src",arrayAboutMePicture[i]);
+    }
+
+
+    
+
+});
+
+
+
+// aboutMePicture.addEventListener("click",function(){
+
 
